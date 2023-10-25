c43c9343cefa98f36d10b2fc6aa6fbca231a12d1
arthur <arthur.trusgnach@epitech.eu>
2023-10-25 16:20:44 +0200

    Working time

--- frontend/src/components/WorkingTime.vue
+++ frontend/src/components/WorkingTime.vue
@@ -1,22 +1,68 @@
 <template>
-    <div class="card">
-      <div class="card-header">
-        <slot name="header">WorkingTime</slot>
-      </div>
-      <div class="card-body">
-      </div>
-      <div class="card-footer">
-        <slot name="footer">Pied de carte</slot>
-      </div>
-    </div>
+          <td>{{ formatTime(workingTime.start) }}</td>
+          <td>{{ formatTime(workingTime.end) }}</td>
+          <td>
+            <button @click="updateWorkingTime(workingTime.id)">Modifier</button>
+            <button @click="deleteWorkingTime(workingTime.id)">Supprimer</button>
+          </td>
 </template>
 
 <script>
-    export default {
-        name: 'WorkingTime'
+import moment from 'moment';
+import { ENDPOINTS } from '../api/endpoints.js'; 
+import { POST, PUT, DELETE } from '../api/axios.js'; 
+
+export default {
+  props: {
+    workingTime: {
+      type: Object,
+      required:true
     }
+  },
+  data() {
+    return {
+      userId: localStorage.getItem('user'),
+    };
+  },
+  methods: {
+    createWorkingTime(newTime) {
+      POST(`${ENDPOINTS.CREATE_TIME}${this.userId}`, newTime)
+        .then(() => {
+          this.getWorkingTimes("2023-01-12T12:45:14Z", "2023-12-12T12:45:14Z");
+        })
+        .catch(() => {
+          console.log('An error occurred while creating the working time');
+        });
+    },
+    updateWorkingTime(timeId) {
+      // Vous devez fournir les données à mettre à jour
+      PUT(`${ENDPOINTS.UPDATE_TIME}${this.userId}/${timeId}`, { /* données à mettre à jour */ })
+        .then(() => {
+          this.getWorkingTimes("2023-01-12T12:45:14Z", "2023-12-12T12:45:14Z");
+        })
+        .catch(() => {
+          console.log('An error occurred while updating the working time');
+        });
+    },
+    deleteWorkingTime(timeId) {
+      DELETE(`${ENDPOINTS.DELETE_TIME}${this.userId}/${timeId}`)
+        .then(() => {
+          this.getWorkingTimes("2023-01-12T12:45:14Z", "2023-12-12T12:45:14Z");
+        })
+        .catch(() => {
+          console.log('An error occurred while deleting the working time');
+        });
+    },
+    formatTime(dateTime) {
+      return moment(dateTime).format('YYYY-MM-DD HH:mm:ss');
+    }
+  },
+  created() {
+    this.getWorkingTimes("2023-01-12T12:45:14Z", "2023-12-12T12:45:14Z");
+  }
+};
 </script>
 
 <style scoped>
-
+/* Add your styles here */
 </style>
--- frontend/src/components/WorkingTimes.vue
+++ frontend/src/components/WorkingTimes.vue
@@ -1,53 +1,61 @@
 <template>
-    <div>
-      <h1>Working Times</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Start Time</th>
-            <th>End Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="time in workingTimes" :key="time.id">
-            <td>{{ time.start }}</td>
-            <td>{{ time.end }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
-  <script>
-  import { ENDPOINTS } from '../api/endpoints.js'; 
-  import { GET } from '../api/axios.js'; 
+  <div>
+    <h1>Working Times</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Start Time</th>
+          <th>End Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="time in workingTimes" :key="time.id">
+          <working-time :working-time="time" />
+          <!-- <td>{{ formatTime(time.start) }}</td>
+          <td>{{ formatTime(time.end) }}</td> -->
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
 
-  export default {
-    data() {
-      return {
-        userId: 'd46350c0-1971-414b-b5d2-90ed541c7255',
-        workingTimes: []
-      };
+<script>
+import moment from 'moment';
+import { ENDPOINTS } from '../api/endpoints.js'; 
+import { GET } from '../api/axios.js'; 
+import WorkingTime from './WorkingTime.vue';
+
+export default {
+  name: 'WorkingTimes',
+  components: {
+    WorkingTime
+  },
+  data() {
+    return {
+      userId: localStorage.getItem('user'),
+      workingTimes: []
+    };
+  },
+  methods: {
+    getWorkingTimes(start, end) {
+      GET(ENDPOINTS.GET_ALL_TIME + this.userId + '?start=' + start + '&end=' + end)
+        .then(response => {
+          this.workingTimes = response.data.data;
+        })
+        .catch(() => {
+          console.log('An error occurred while fetching the working times');
+        });
     },
-    methods: {
-      getWorkingTimes(start, end) {
-        GET(ENDPOINTS.GET_ALL_TIME + this.userId+'?start=' +start +'&end=' +end)
-          .then(response => {
-            this.workingTimes = response.data.data;
-            console.log(response.data.data)
-          })
-          .catch(() => {
-            console.log('An error occurred while fetching the working times');
-          });
-      }
-    },
-    created() {
-      this.getWorkingTimes("2023-01-12T12:45:14Z", "2023-12-12T12:45:14Z");
+    formatTime(dateTime) {
+      return moment(dateTime).format('YYYY-MM-DD HH:mm:ss');
     }
-  };
-  </script>
-  
-  <style scoped>
-  /* Add your styles here */
-  </style>
-  
+  },
+  created() {
+    this.getWorkingTimes("2023-01-12T12:45:14Z", "2023-12-12T12:45:14Z");
+  }
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
