50cd88bd555023cfbe5e927bc7528b9e88731add
arthur <arthur.trusgnach@epitech.eu>
2023-10-25 14:38:15 +0200

    workingTime

--- frontend/src/components/WorkingTimes.vue
+++ frontend/src/components/WorkingTimes.vue
@@ -1,15 +1,53 @@
 <template>
     <div>
-        WorkingTimes
+      <h1>Working Times</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Start Time</th>
+            <th>End Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="time in workingTimes" :key="time.id">
+            <td>{{ time.start }}</td>
+            <td>{{ time.end }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-</template>
+  </template>
+  
+  <script>
+  import { ENDPOINTS } from '../api/endpoints.js'; 
+  import { GET } from '../api/axios.js'; 
 
-<script>
-    export default {
-        name: 'WorkingTimes'
+  export default {
+    data() {
+      return {
+        userId: 'd46350c0-1971-414b-b5d2-90ed541c7255',
+        workingTimes: []
+      };
+    },
+    methods: {
+      getWorkingTimes(start, end) {
+        GET(ENDPOINTS.GET_ALL_TIME + this.userId+'?start=' +start +'&end=' +end)
+          .then(response => {
+            this.workingTimes = response.data.data;
+            console.log(response.data.data)
+          })
+          .catch(() => {
+            console.log('An error occurred while fetching the working times');
+          });
+      }
+    },
+    created() {
+      this.getWorkingTimes("2023-01-12T12:45:14Z", "2023-12-12T12:45:14Z");
     }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
