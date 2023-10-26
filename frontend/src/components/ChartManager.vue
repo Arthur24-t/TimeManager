ecbe2318afaa0dbd7ecc5861c9e40e7e698f7a0f
DESKTOP-4E6IIOU\Bastien R <rivart.bastien@hotmail.fr>
2023-10-26 17:20:25 +0200

    add the chart manager

--- frontend/src/components/ChartManager.vue
+++ frontend/src/components/ChartManager.vue
@@ -1,26 +1,18 @@
 <template>
   <div>
     <ChartBar />
-  </div>
-  <div>
     <LinearChart />
   </div>
 </template>
  
 <script>
 import ChartBar from './ChartBar.vue';
+import LinearChart from './LinearChart.vue';
 
 export default {
   name: 'ChartManager',
   components: {
-    ChartBar
-  }
-};
-import LinearChart from './LinearChart.vue';
- 
-export default {
-  name: 'ChartManager',
-  components: {
+    ChartBar,
     LinearChart
   }
 };
