3b39990290bbbc3cf75ee11d35073f9226b38424
Arthur <91197891+Arthur24-t@users.noreply.github.com>
2023-10-29 17:03:34 +0100

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'timemanager.freeboxos.fr:4000',
   headers: {
     'Content-Type': 'application/json',
   },
